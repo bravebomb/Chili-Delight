246cb8a41793e9380b7170bcde198600059dbef1
bravebomb <ane.delalic@hotmail.se>
2024-11-03 12:27:29 +0100

    seed texture and loot tables work! #1 #4

--- src/main/java/com/bravebomb/chilidelight/client/ChiliDelightClient.java
+++ src/main/java/com/bravebomb/chilidelight/client/ChiliDelightClient.java
@@ -1,13 +1,21 @@
 package com.bravebomb.chilidelight.client;
 
 import com.bravebomb.chilidelight.common.registry.CDBlocks;
+import io.github.fabricators_of_create.porting_lib.config.ConfigRegistry;
+import io.github.fabricators_of_create.porting_lib.config.ConfigType;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.*;
+import vectorwing.farmersdelight.common.Configuration;
+
 
 public class ChiliDelightClient implements ClientModInitializer {
+    public static final String MODID = "chiilidelight";
+
+
     @Override
     public void onInitializeClient() {
+        ConfigRegistry.registerConfig(MODID, ConfigType.CLIENT, Configuration.CLIENT_CONFIG);
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), CDBlocks.BEANS_BLOCK.get());
     }
 }
